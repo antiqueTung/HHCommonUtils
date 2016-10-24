56018542077055c2fa2d8b1ef14c764a3cd15391
AntiqueTung <13403690960@139.com>
2016-10-24 11:59:44 +0800

    cover a project
    my project

---
+++ HHCommonUtils/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HHCommonUtils
+//
+//  Created by thx03 on 16/10/24.
+//  Copyright © 2016年 antique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HHCommonUtils/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HHCommonUtils
+//
+//  Created by thx03 on 16/10/24.
+//  Copyright © 2016年 antique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
